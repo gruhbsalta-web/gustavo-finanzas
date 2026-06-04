409ceec8174f63ebac295972d993203a851696b2
gruhbsalta-web <gruhbsalta@gmail.com>
2026-06-03 21:59:17 -0300

    Add service worker for PWA install

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME='mis-finanzas-v3.7';
+const ASSETS=['./','./index.html','./manifest.json'];
+
+self.addEventListener('install',e=>{
+    e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+    e.waitUntil(caches.keys().then(keys=>
+        Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))
+    ));
+    self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+    e.respondWith(
+        fetch(e.request).then(r=>{
+            if(r.ok){const c=r.clone();caches.open(CACHE_NAME).then(cache=>cache.put(e.request,c))}
+            return r;
+        }).catch(()=>caches.match(e.request))
+    );
+});
